7d339546d09ed1880b4b0308bf8bcb5ba382557f
Everett Hanke <forgetekbusiness@gmail.com>
2024-05-16 14:39:11 -0400

    :x

--- classes/pet.php
+++ classes/pet.php
@@ -14,4 +14,37 @@ class Pet
         $this->_animal = $_animal;
         $this->_color = $_color;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getAnimal()
+    {
+        return $this->_animal;
+    }
+
+    /**
+     * @param mixed $animal
+     */
+    public function setAnimal($animal): void
+    {
+        $this->_animal = $animal;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->_color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color): void
+    {
+        $this->_color = $color;
+    }
+
 }
